6685735d451186f274ed046f23d079f89d81c082
Leonardo Canales <leonardo.canales@clearesult.com>
2021-12-02 19:00:12 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require('tailwindcss/colors');
+delete colors['lightBlue'];
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors,
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
