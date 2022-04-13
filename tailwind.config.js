3f07372d80b3092b830bd89c578a1be3fa280be8
Leonardo canales <tiestomike@gmail.com>
2022-04-13 18:31:13 -0500

    Setting up prettier and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 const colors = require('tailwindcss/colors');
 delete colors['lightBlue'];
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors,
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors,
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
