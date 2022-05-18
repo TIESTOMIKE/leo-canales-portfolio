06f85df279a472babbbdfc4596808f53278b845e
Leonardo canales <tiestomike@gmail.com>
2022-05-18 17:43:48 -0500

    Adding key prop

--- components/AnimatedTitle.js
+++ components/AnimatedTitle.js
@@ -18,7 +18,10 @@ export default function AnimatedTitle() {
 		<div className="flex flex-col content-center m-auto w-fit">
 			{titleElements.map(el => {
 				return (
-					<div className="flex flex-row align-middle items-baseline">
+					<div
+						key={el.letter}
+						className="flex flex-row align-middle items-baseline"
+					>
 						<h1 className="m-0">{el.letter}</h1>
 						<h3 className="m-0">{el.firstDesc}</h3>
 					</div>
